30e8eccf24da005e9a8d11573ceba0d7496668ca
Antony Du <594153885@qq.com>
2018-04-22 21:34:13 +0800

    add docker

---
+++ docker/env/apache-php70/app/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Hello world!</title>
+	<style>
+	body {
+		background-color: white;
+		text-align: center;
+		padding: 50px;
+		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+	}
+
+	#logo {
+		margin-bottom: 40px;
+	}
+	</style>
+</head>
+<body>
+<?php
+phpinfo();
+?>
+</body>
+</html>
+
